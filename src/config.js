39932b7fafd297b4633084c34084bdbd57665ab7
Marco Friaz <marfriaz@gmail.com>
2020-10-08 00:07:26 -0700

    users route ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  // DATABASE_URL:
+  //   process.env.DATABASE_URL || "postgresql://gymbnb_admin@localhost/gymbnb",
+  // TEST_DATABASE_URL:
+  //   process.env.DATABASE_URL ||
+  //   "postgresql://gymbnb_admin@localhost/gymbnb-test",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
 };
--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,13 @@
+process.env.TZ = "UCT";
+process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = "test-jwt-secret";
+
+require("dotenv").config();
+
+// process.env.TEST_DATABASE_URL =
+//   process.env.TEST_DATABASE_URL ||
+//   "postgresql://gymbnb_admin@localhost/gymbnb-test";
+
 const { expect } = require("chai");
 const supertest = require("supertest");
 
