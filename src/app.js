a44fa477ba31cac7b8726f1150bea7009dc19db5
Marco Friaz <marfriaz@gmail.com>
2021-03-23 23:02:55 -0700

    updated cors

--- src/app.js
+++ src/app.js
@@ -17,23 +17,14 @@ app.use(
     skip: () => NODE_ENV === "test",
   })
 );
+app.use(cors());
+app.use(helmet());
 
-// app.use(cors({ origin: false }));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use("/api/auth", AuthRouter);
 app.use("/api/accounts", AccountsRouter);
 app.use("/api/addresses", AddressRouter);
 app.use("/api/notes", NotesRouter);
 app.use("/api/contacts", ContactsRouter);
+app.use("/api/auth", AuthRouter);
 app.use("/api/users", UsersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
