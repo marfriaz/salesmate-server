7fd5cb89d7b469c592efc92b73d4d0408ab5d1c7
Marco Friaz <marfriaz@gmail.com>
2021-03-24 21:38:09 -0700

    cors fix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const ContactsRouter = require("./contacts/contacts-router");
 const AuthRouter = require("./auth/auth-router");
 const UsersRouter = require("./users/users-router");
 
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 const app = express();
 
 app.use(
